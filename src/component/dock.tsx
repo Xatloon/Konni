42531c2d31f0b5448c1989732d88559c70782000
Xat <i@xatloon.me>
2023-10-20 02:40:36 +0800

    style: adjust dock padding

--- src/component/dock.tsx
+++ src/component/dock.tsx
@@ -1,6 +1,6 @@
 export default function Dock() {
   return (
-    <nav className="flex px-4 pt-4 pb-6 fixed bottom-0 w-full bg-slate-50/70 dark:bg-slate-950/70 backdrop-blur">
+    <nav className="flex items-center p-4 fixed bottom-0 w-full bg-slate-50/70 dark:bg-slate-950/70 backdrop-blur">
       <div className="flex-grow text-center">
         <span className="i-tabler-home text-2xl" />
       </div>
